db7183d4c81b2c8396755a557220558322835574
Shubham Yadav <subhamyadav580@gmail.com>
2021-03-11 02:32:22 +0530

    accepting friend request

--- src/friend/urls.py
+++ src/friend/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from friend.views import(
 	send_friend_request,
     friend_requests,
+    accept_friend_request,
 )
 
 app_name = 'friend'
@@ -10,5 +11,6 @@ app_name = 'friend'
 urlpatterns = [
     path('friend_request/', send_friend_request, name='friend-request'),
     path('friend_requests/<user_id>/', friend_requests, name='friend-requests'),
+    path('accept_friend_request/<friend_request_id>/', accept_friend_request, name='friend-request-accept'),
 
 ]
