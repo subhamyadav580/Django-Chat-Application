f56694a1524b6c7f706df016b25a89a43f92e77b
Shubham Yadav <subhamyadav580@gmail.com>
2021-03-11 02:57:24 +0530

    decline friend request

--- src/friend/urls.py
+++ src/friend/urls.py
@@ -5,6 +5,7 @@ from friend.views import(
     friend_requests,
     accept_friend_request,
     remove_friend,
+    decline_friend_request,
 )
 
 app_name = 'friend'
@@ -14,5 +15,7 @@ urlpatterns = [
     path('friend_requests/<user_id>/', friend_requests, name='friend-requests'),
     path('accept_friend_request/<friend_request_id>/', accept_friend_request, name='friend-request-accept'),
 	path('friend_remove/', remove_friend, name='remove-friend'),
+    path('friend_request_decline/<friend_request_id>/', decline_friend_request, name='friend-request-decline'), # TODO: DELETE THIS LINE.
+
 
 ]
